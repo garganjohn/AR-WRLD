279fc14c10eeb21ca43af556c8ca913ca5607889
garganjohn <garganjohn@gmail.com>
2019-05-11 17:19:37 -0400

    second commit, added base project structure and methods

--- app/src/main/java/org/pursuit/ar_wrld/MainActivity.java
+++ app/src/main/java/org/pursuit/ar_wrld/MainActivity.java
@@ -1,13 +1,156 @@
 package org.pursuit.ar_wrld;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Trackable;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.PlaneRenderer;
+import com.google.ar.sceneform.rendering.Texture;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ArFragment arFragment;
+    private ModelLoader modelLoader;
+    private int modelLives = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        modelLoader = new ModelLoader(new WeakReference<>(this));
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.sceneform_fragment);
+
+        arFragment.getPlaneDiscoveryController().hide();
+        arFragment.getPlaneDiscoveryController().setInstructionView(null);
+
+        arFragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
+            arFragment.onUpdate(frameTime);
+
+        });
+        changetexture();
+        initializeGallery();
     }
+
+    private android.graphics.Point getScreenCenter() {
+        View vw = findViewById(android.R.id.content);
+        return new android.graphics.Point(vw.getWidth() / 2, vw.getHeight() / 2);
+    }
+
+    private void initializeGallery() {
+        LinearLayout gallery = findViewById(R.id.gallery_layout);
+
+        ImageView andy = new ImageView(this);
+        andy.setImageResource(R.drawable.droid_thumb);
+        andy.setContentDescription("andy");
+        andy.setOnClickListener(view -> {
+            addObject(Uri.parse("andy.sfb"));
+        });
+        gallery.addView(andy);
+
+        ImageView cabin = new ImageView(this);
+        cabin.setImageResource(R.drawable.cabin_thumb);
+        cabin.setContentDescription("cabin");
+        cabin.setOnClickListener(view -> {
+            addObject(Uri.parse("Cabin.sfb"));
+        });
+        gallery.addView(cabin);
+
+        ImageView house = new ImageView(this);
+        house.setImageResource(R.drawable.house_thumb);
+        house.setContentDescription("house");
+        house.setOnClickListener(view -> {
+            addObject(Uri.parse("House.sfb"));
+        });
+        gallery.addView(house);
+
+        ImageView igloo = new ImageView(this);
+        igloo.setImageResource(R.drawable.igloo_thumb);
+        igloo.setContentDescription("igloo");
+        igloo.setOnClickListener(view -> {
+            addObject(Uri.parse("igloo.sfb"));
+        });
+        gallery.addView(igloo);
+    }
+
+    private void addObject(Uri model) {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+        android.graphics.Point pt = getScreenCenter();
+        List<HitResult> hits;
+        if (frame != null) {
+            hits = frame.hitTest(pt.x, pt.y);
+            for (HitResult hit : hits) {
+                Trackable trackable = hit.getTrackable();
+                if (trackable instanceof Plane &&
+                        ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
+                    modelLoader.loadModel(hit.createAnchor(), model);
+                    break;
+
+                }
+            }
+        }
+    }
+
+    public void addNodeToScene(Anchor anchor, ModelRenderable renderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+        node.setRenderable(renderable);
+        node.setParent(anchorNode);
+        node.setLocalPosition(new Vector3(0f, 0f, -2f));
+        arFragment.getArSceneView().getScene().addChild(anchorNode);
+        node.setOnTapListener((hitTestResult, motionEvent) -> {
+            if (1 < modelLives) {
+                modelLives--;
+            } else {
+                anchor.detach();
+            }
+            Toast.makeText(MainActivity.this, "MODEL HAS " + modelLives + " LIVES LEFT!", Toast.LENGTH_SHORT).show();
+        });
+        node.select();
+    }
+
+    public void onException(Throwable throwable) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(throwable.getMessage())
+                .setTitle("Codelab error!");
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        return;
+    }
+
+    public void changetexture() {
+        Texture.Sampler sampler =
+                Texture.Sampler.builder()
+                        .setMinFilter(Texture.Sampler.MinFilter.LINEAR)
+                        .setWrapMode(Texture.Sampler.WrapMode.REPEAT)
+                        .build();
+        Texture.builder()
+                .setSource(this, R.drawable.testing_carpet_texture)
+                .setSampler(sampler)
+                .build()
+                .thenAccept(texture -> {
+                    arFragment.getArSceneView().getPlaneRenderer()
+                            .getMaterial().thenAccept(material ->
+                            material.setTexture(PlaneRenderer.MATERIAL_TEXTURE, texture));
+                });
+    }
+
 }
