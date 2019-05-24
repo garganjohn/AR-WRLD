529544fc32e3ecfc4f25cda6f90e1cdd367ad70a
ccharle <cliffordcharles@pursuit.org>
2019-05-24 15:26:02 -0400

    save changes

---
+++ app/src/main/java/org/pursuit/ar_wrld/movement/ModelSpeed.java
@@ -0,0 +1,26 @@
+package org.pursuit.ar_wrld.movement;
+
+public class ModelSpeed {
+
+
+    private float modelSpeedMultiplier = 1.0f;
+    private float rotationSpeedMultiplier = 1.0f;
+
+    public void setModelSpeedMultiplier(float modelSpeedMultiplier) {
+        this.modelSpeedMultiplier = modelSpeedMultiplier;
+    }
+
+    public float getModelSpeedMultiplier() {
+        return modelSpeedMultiplier;
+    }
+
+    public void setRotationSpeedMultiplier(float rotationSpeedMultiplier) {
+        this.rotationSpeedMultiplier = rotationSpeedMultiplier;
+    }
+
+    public float getRotationSpeedMultiplier() {
+        return rotationSpeedMultiplier;
+    }
+}
+
+}
