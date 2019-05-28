6e8fe343e56987a398f227d7729c84f4e973fe79
khaisoe <khaingsoe@pursuit.org>
2019-05-28 18:06:04 -0400

    added skeletons for recyclerview

---
+++ app/src/main/java/org/pursuit/ar_wrld/recyclerview/TopScoreAdapter.java
@@ -0,0 +1,23 @@
+package org.pursuit.ar_wrld.recyclerview;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class TopScoreAdapter extends RecyclerView.Adapter<TopScoreViewHolder> {
+    @NonNull
+    @Override
+    public TopScoreViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TopScoreViewHolder topScoreViewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/org/pursuit/ar_wrld/recyclerview/TopScoreViewHolder.java
@@ -0,0 +1,11 @@
+package org.pursuit.ar_wrld.recyclerview;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class TopScoreViewHolder extends RecyclerView.ViewHolder {
+    public TopScoreViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
