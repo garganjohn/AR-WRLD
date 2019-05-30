b8b0aa12e10ec4e6e421c756f855ee3ab601a889
garganjohn <garganjohn@gmail.com>
2019-05-30 18:36:15 -0400

    made nav and status bar transparent and made splash_bg centerCrop to account for diff. screen sizes

--- app/src/main/java/org/pursuit/ar_wrld/SplashActivity.java
+++ app/src/main/java/org/pursuit/ar_wrld/SplashActivity.java
@@ -1,17 +1,16 @@
 package org.pursuit.ar_wrld;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Handler;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import org.pursuit.ar_wrld.login.SignInActivity;
 
 public class SplashActivity extends AppCompatActivity {
-    private static final int SPLASH_LENGTH = 3000;
+    private static final int SPLASH_LENGTH = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +20,6 @@ public class SplashActivity extends AppCompatActivity {
         changeStatusBarColor();
 
         new Handler().postDelayed(() -> {
-            /* Create an Intent that will start the Menu-Activity. */
             Intent mainIntent = new Intent(SplashActivity.this, SignInActivity.class);
             SplashActivity.this.startActivity(mainIntent);
             SplashActivity.this.finish();
@@ -32,10 +30,8 @@ public class SplashActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
     }
-    private void changeStatusBarColor(){
-        if (Build.VERSION.SDK_INT >= 21) {
-            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.colorAccent)); // Navigation bar the soft bottom of some phones like nexus and some Samsung note series
-            getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.colorAccent)); //status bar or the time bar at the top
-        }
+
+    private void changeStatusBarColor() {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS); //Makes both status and navbar transparent
     }
 }
