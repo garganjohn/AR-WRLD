fcb98594b947f22a699efdf116a41451d6dccffb
ccharle <cliffordcharles@pursuit.org>
2019-05-30 14:14:28 -0400

    fixing class

--- app/src/main/java/org/pursuit/ar_wrld/Effects/AudioLoader.java
+++ app/src/main/java/org/pursuit/ar_wrld/Effects/AudioLoader.java
@@ -2,8 +2,9 @@ package org.pursuit.ar_wrld.Effects;
 
 import android.content.Context;
 import android.media.MediaPlayer;
-import android.net.Uri;
-import android.widget.Button;
+
+
+import org.pursuit.ar_wrld.R;
 
 /**
  * MediaPlayer.create needs context, and R.raw.nameofsound
@@ -17,6 +18,16 @@ public class AudioLoader {
     private MediaPlayer shootingSound;
     private MediaPlayer pointUp;
     private MediaPlayer miss;
+
+    public MediaPlayer getMediaPlayer() {
+        return mediaPlayer;
+    }
+
+    public void setMediaPlayer(MediaPlayer mediaPlayer) {
+        this.mediaPlayer = mediaPlayer;
+    }
+
+    private MediaPlayer mediaPlayer;
 
     public AudioLoader(Context context) {
         this.context = context;
@@ -44,5 +55,39 @@ public class AudioLoader {
 
     public MediaPlayer getMiss() {
         return miss;
+    }
+
+
+    public void explodeSound(){
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer = MediaPlayer.create(context, R.raw.explosion8bit);
+        mediaPlayer.seekTo(0);
+        mediaPlayer.setVolume(20f,20f);
+        mediaPlayer.start();
+
+
+    }
+    public void laserSound(){
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer = MediaPlayer.create(context, R.raw.laserbeam);
+        mediaPlayer.seekTo(0);
+        mediaPlayer.setVolume(20f,20f);
+        mediaPlayer.start();
+
+
+    }
+    public void backGroundMusic(){
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer = MediaPlayer.create(context, R.raw.backgroundmusic);
+        mediaPlayer.seekTo(0);
+        mediaPlayer.setVolume(20f,20f);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+
+    }
+
+    public void stopAudio(){
+
+        mediaPlayer.release();
     }
 }
