de5559cad7bef1109fed1af07bcc4cc58b09812e
enricky13 <enriquecruz@pursuit.com>
2019-06-01 12:05:44 -0400

    Viewpager for passive option and models spawn infront of user

---
+++ app/src/main/java/org/pursuit/ar_wrld/viewPager/ViewPagerAdapter.java
@@ -0,0 +1,29 @@
+package org.pursuit.ar_wrld.viewPager;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter{
+    List<Fragment> fragmentList;
+
+    public ViewPagerAdapter(FragmentManager fm, List<Fragment> fragmentList){
+        super(fm);
+        this.fragmentList = fragmentList;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+}
