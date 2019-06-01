b6c5ac170b5ff564091f50b4651662b897da93a1
enricky13 <enriquecruz@pursuit.com>
2019-06-01 10:54:08 -0400

    small edits and changes

--- app/src/main/java/org/pursuit/ar_wrld/GameInformation.java
+++ app/src/main/java/org/pursuit/ar_wrld/GameInformation.java
@@ -4,8 +4,8 @@ public interface GameInformation {
     String SHARED_PREF_KEY = "shared_pref_key";
     String USER_SCORE_KEY = "user_score";
     String EASY_ENEMY = "CUPIC_FYINGSAUCER.sfb";
-    String MEDIUM_ENEMY = "donut.sfb";
-    String HARD_ENEMY = "spaceship.sfb";
+    String MEDIUM_ENEMY = "spaceship.sfb";
+    String HARD_ENEMY = "donut.sfb";
     String BOSS_ENEMY = "FlyingSacuer.sfb";
     String TIME_INCREASE_MODEL = "1345 Analog Clock.sfb";
     String GAME_DIFFICULTY = "GAME DIFFICULTY";
